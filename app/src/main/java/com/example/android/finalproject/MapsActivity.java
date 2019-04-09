03b3aeb209be4f155367a9f3de12d5651966dbbd
tony-albanese <tony.albanese2000@gmail.com>
2019-04-09 18:16:38 +0200

    Added user permission request to get location.

--- app/src/main/java/com/example/android/finalproject/MapsActivity.java
+++ app/src/main/java/com/example/android/finalproject/MapsActivity.java
@@ -1,18 +1,60 @@
 package com.example.android.finalproject;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.mapquest.mapping.MapQuest;
 
 public class MapsActivity extends AppCompatActivity {
 
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         MapQuest.start(getApplicationContext());
+        requestPermission();
     }
 
+
+    public void requestPermission() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        } else {
+
+        }
+    }
+
+
+    //This function will handle what to do when the user sets the permission. Don't touch it.
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+                // If the permission is granted, get the location,
+                // otherwise, show a Toast
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLastLocation();
+                } else {
+                    finish();
+                }
+                break;
+        }
+    }
+
+
+    public void getLastLocation() {
+        Toast.makeText(this, "Permission granted", Toast.LENGTH_LONG).show();
+    }
 }
