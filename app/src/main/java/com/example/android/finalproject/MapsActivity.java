0658ce7ea5938ab23f968cb62f6e777d5d6c1ec1
tony-albanese <tony.albanese2000@gmail.com>
2019-04-09 17:25:46 +0200

    Removed references to Google Maps.

--- app/src/main/java/com/example/android/finalproject/MapsActivity.java
+++ app/src/main/java/com/example/android/finalproject/MapsActivity.java
@@ -1,25 +1,15 @@
 package com.example.android.finalproject;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
 public class MapsActivity extends AppCompatActivity {
-
-    private GoogleMap mMap;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
     }
-
 
 }
