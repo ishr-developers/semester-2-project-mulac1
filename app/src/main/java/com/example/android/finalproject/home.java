d38a208ae240afff564caa78e4bcecf929ac3490
tony-albanese <tony.albanese2000@gmail.com>
2019-04-09 17:40:40 +0200

    Changed intent.

--- app/src/main/java/com/example/android/finalproject/home.java
+++ app/src/main/java/com/example/android/finalproject/home.java
@@ -1,8 +1,8 @@
 package com.example.android.finalproject;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,7 +17,7 @@ public class home extends AppCompatActivity {
 
         search_button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                startActivity(new Intent (home.this, MapsActivity.class));
+                startActivity(new Intent(getApplicationContext(), MapsActivity.class));
             }
         });
     }
