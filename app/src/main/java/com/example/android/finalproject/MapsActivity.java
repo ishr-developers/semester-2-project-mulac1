ebfd65d3c342ae2f03692dfd575e854442c56b3c
tony-albanese <tony.albanese2000@gmail.com>
2019-05-09 10:52:01 +0200

    Added whitespace.

--- app/src/main/java/com/example/android/finalproject/MapsActivity.java
+++ app/src/main/java/com/example/android/finalproject/MapsActivity.java
@@ -30,8 +30,6 @@ public class MapsActivity extends AppCompatActivity {
     //TODO: Create a function to search MapQuest
 
 
-
-
     public void requestPermission() {
         if (ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
