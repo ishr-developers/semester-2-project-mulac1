d8295ed6f6cf9f0be5c699fe7b8f39a2416a394d
mulac1 <calum-morton@is-hr.de>
2019-05-09 13:40:55 +0200

    Commit files.

--- app/src/main/java/com/example/android/finalproject/MapsActivity.java
+++ app/src/main/java/com/example/android/finalproject/MapsActivity.java
@@ -1,11 +1,14 @@
 package com.example.android.finalproject;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.mapquest.mapping.MapQuest;
@@ -20,6 +23,14 @@ public class MapsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_maps);
         MapQuest.start(getApplicationContext());
         requestPermission();
+
+        Button list_button =(Button) findViewById(R.id.listButton);
+
+        list_button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(getApplicationContext(), ListActivtiy.class));
+            }
+        });
     }
 
 
